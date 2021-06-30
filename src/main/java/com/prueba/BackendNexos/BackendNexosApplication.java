7bab935cec38d3175531406f67d00f77f77d1475
TamayoA <hftamayoa@corhuila.edu.co>
2021-06-30 14:29:29 -0500

    Feat: Proyecto inicial

---
+++ src/main/java/com/prueba/BackendNexos/BackendNexosApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.BackendNexos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNexosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNexosApplication.class, args);
+	}
+
+}
