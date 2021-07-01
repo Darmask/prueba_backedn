0fb91eb297041267acab6a49979b3c868c7aa7da
TamayoA <hftamayoa@corhuila.edu.co>
2021-07-01 11:38:07 -0500

    Chore: Configuracion de los Beans

--- src/main/java/com/prueba/BackendNexos/BackendNexosApplication.java
+++ src/main/java/com/prueba/BackendNexos/BackendNexosApplication.java
@@ -1,13 +1,32 @@
 package com.prueba.BackendNexos;
 
+import javax.validation.Validator;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableJpaAuditing
 public class BackendNexosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendNexosApplication.class, args);
+	}
+
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
