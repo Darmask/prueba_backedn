765c7836310d4c6347f111ec574473214cc2c037
TamayoA <hftamayoa@corhuila.edu.co>
2021-07-01 11:37:19 -0500

    Test: Pruebas en el servicio de cargo y mercancia

---
+++ src/test/java/com/prueba/BackendNexos/BackendNexosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.BackendNexos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNexosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
