5f5e81f0411fa0494c020883657f43620f2a50a1
TamayoA <hftamayoa@corhuila.edu.co>
2021-07-01 08:52:10 -0500

    Feat: Registrar y listar cargos

---
+++ src/main/java/com/prueba/BackendNexos/Dto/MensajeError.java
@@ -0,0 +1,14 @@
+package com.prueba.BackendNexos.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Data
+public class MensajeError {
+
+  private String mensaje;
+
+}
